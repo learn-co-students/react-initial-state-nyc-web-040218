3ddfad04dccf522d29d1e47b2fb275d9e6e7b374
briecodes <hello@brie.work>
2018-06-05 13:52:23 -0400

    empty project.

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,9 @@
-// Component Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+import ToggleButton from './components/ToggleButton';
+ 
+ReactDOM.render(
+  <ToggleButton />,
+  document.getElementById('root')
+);
